cf24d967aefecb289869777e95a558b67c575923
ruanfonseca <whitelook22@outlook.com>
2024-01-19 11:25:57 -0300

    corrigindo integração com rabbitmq

--- src/main/java/com/ServicePro/ServicePro/template/TemplateMSG.java
+++ src/main/java/com/ServicePro/ServicePro/template/TemplateMSG.java
@@ -1,18 +1,34 @@
 package com.ServicePro.ServicePro.template;
 
-
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
-@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class TemplateMSG implements Serializable {
 
+   private static final long serialVersionUID = 1L;
 
-   public static long serialVersionUID = 1L;
    private String MSG;
    private String EMAIL;
    private String NOME;
+
+   @JsonCreator
+   public TemplateMSG(@JsonProperty("MSG") String MSG,
+                      @JsonProperty("EMAIL") String EMAIL,
+                      @JsonProperty("NOME") String NOME) {
+      this.MSG = MSG;
+      this.EMAIL = EMAIL;
+      this.NOME = NOME;
+   }
+
 
 
    @Override
