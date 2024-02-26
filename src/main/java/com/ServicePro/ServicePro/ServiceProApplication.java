0f7cd5f2f412f20ead38db6614994645e8b8fd68
Ruan Fonseca <64973978+Ruanfonseca@users.noreply.github.com>
2024-02-26 14:35:41 -0300

    corrigindo problemas das queues

--- src/main/java/com/ServicePro/ServicePro/ServiceProApplication.java
+++ src/main/java/com/ServicePro/ServicePro/ServiceProApplication.java
@@ -2,8 +2,10 @@ package com.ServicePro.ServicePro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.ServicePro.ServicePro.mensageria")
 public class ServiceProApplication {
 
     public static void main(String[] args) {
