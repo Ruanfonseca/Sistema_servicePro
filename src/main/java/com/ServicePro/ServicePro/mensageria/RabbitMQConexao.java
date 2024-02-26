32708986bd9f3eec188612376f44cf28c0cde5ce
Ruan Fonseca <64973978+Ruanfonseca@users.noreply.github.com>
2024-02-26 15:35:22 -0300

    tentando 4

--- src/main/java/com/ServicePro/ServicePro/mensageria/RabbitMQConexao.java
+++ src/main/java/com/ServicePro/ServicePro/mensageria/RabbitMQConexao.java
@@ -1,51 +1,47 @@
 package com.ServicePro.ServicePro.mensageria;
-
-
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
-public class RabbitMQConexao {
+public class RabbitMQConexao implements ApplicationRunner {
 
     private static final String NOME_EXCHANGE = "amq.direct";
 
-    private AmqpAdmin amqpAdmin;
+    private final AmqpAdmin amqpAdmin;
 
-    public RabbitMQConexao(AmqpAdmin amqpAdmin){
+    public RabbitMQConexao(AmqpAdmin amqpAdmin) {
         this.amqpAdmin = amqpAdmin;
     }
 
-    private Queue fila (String nomeFila){
-        return new Queue(nomeFila,true,false,false);
+
+    private Queue fila(String nomeFila) {
+        return new Queue(nomeFila, true, false, false);
     }
 
-
-    //CANAL POR ONDE AS MENSAGENS DA FILA SERÃO ENVIADAS
-    private DirectExchange envioDeMsg(){
+    private DirectExchange envioDeMsg() {
         return new DirectExchange(NOME_EXCHANGE);
     }
-    private Binding relacionamento(Queue fila,DirectExchange enviomsg){
-        return new Binding(fila.getName(),Binding.DestinationType.QUEUE,enviomsg.getName(), fila.getName(),null);
+
+    private Binding relacionamento(Queue fila, DirectExchange enviomsg) {
+        return new Binding(fila.getName(), Binding.DestinationType.QUEUE, enviomsg.getName(), fila.getName(), null);
     }
 
-
-
-    @PostConstruct
-    private void adiciona(){
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         //preparando as filas
         Queue filaRequerimentoWIFI = this.fila(RabbitmqConstantes.FILA_WIFI);
         Queue filaRequerimentoSALA = this.fila(RabbitmqConstantes.FILA_SALA);
         Queue filaRequerimentoProjetor = this.fila(RabbitmqConstantes.FILA_PROJETOR);
         DirectExchange enviomsg = this.envioDeMsg();
 
-        Binding LigacaoWIFI = this.relacionamento(filaRequerimentoWIFI,enviomsg);
-        Binding LigacaoSALA = this.relacionamento(filaRequerimentoSALA,enviomsg);
-        Binding LigacaoPROJETOR = this.relacionamento(filaRequerimentoProjetor,enviomsg);
+        Binding LigacaoWIFI = this.relacionamento(filaRequerimentoWIFI, enviomsg);
+        Binding LigacaoSALA = this.relacionamento(filaRequerimentoSALA, enviomsg);
+        Binding LigacaoPROJETOR = this.relacionamento(filaRequerimentoProjetor, enviomsg);
 
         //criando as filas no RabbitMQ
         this.amqpAdmin.declareQueue(filaRequerimentoWIFI);
@@ -55,13 +51,8 @@ public class RabbitMQConexao {
         //canal onde as filas estarão
         this.amqpAdmin.declareExchange(enviomsg);
 
-
-
-
         this.amqpAdmin.declareBinding(LigacaoWIFI);
         this.amqpAdmin.declareBinding(LigacaoSALA);
         this.amqpAdmin.declareBinding(LigacaoPROJETOR);
-
     }
-
 }
